e5e7b23ce4629251345d88ca92320b4ba9080c76
Konstantin Zhiliaev <k926227@gmail.com>
2024-05-26 22:08:30 +0300

    Добавил ссылку на админку

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Привет Галя ;)
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/strapi/admin"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Админка
         </a>
       </header>
     </div>
